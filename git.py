fccc147580d3d91ab6757c66abb82ffd4a49bb20
Nate Larsen <nlarse17@imaal10.cs.byu.edu>
2022-02-02 15:37:52 -0700

    test

--- git.py
+++ git.py
@@ -1,6 +1,6 @@
-from time import sleep
-import git
+# import git
 import time
+import subprocess
 
 
 iter = 0
@@ -11,6 +11,16 @@ while 1:
         first = str(iter) + "\n"
         the_file.write(first)
 
-    
+    # repo = git.Repo('/Nlarseny/git_test')
+    # repo.git.add('--all')  # to add all the working files.
+    # repo.git.commit('-m', 'commit message from python script', author='test_user@test.com')
+    # origin = repo.remote(name='origin')
+    # origin.push()
 
-    time.sleep(20)
+    print(subprocess.run(["ls", "-l"]))
+    subprocess.run(["git", "add", "."])
+    subprocess.run(["git", "commit", "-m", "test"])
+    subprocess.run(["git", "push"])
+
+
+    time.sleep(5)
